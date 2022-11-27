fe926802dce40012b6311fc673960ab024a57a83
WWWolverine <tevrizovvv@gmail.com>
2022-11-27 19:17:39 +0600

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { CoinDetailComponent } from './components/coin-detail/coin-detail.compon
 import { CoinListComponent } from './components/coin-list/coin-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'coin-list', pathMatch: 'full' },
-  { path: 'coin-list', component: CoinListComponent },
+  { path: '', component: CoinListComponent },
   { path: 'coin-detail/:id', component: CoinDetailComponent },
 ];
 
